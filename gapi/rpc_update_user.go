829ccd1dad809696fe7705b8f0d83f2e73ba9605
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-04-06 13:16:48 +0100

    chapter 51: authorization to protect gRPC api

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -19,8 +19,17 @@ func (server *Server) UpdateUser(
 	req *pb.UpdateUserRequest,
 ) (*pb.UpdateUserResponse, error) {
 
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	if violations := validateUpdateUserRequest(req); violations != nil {
 		return nil, InvalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
